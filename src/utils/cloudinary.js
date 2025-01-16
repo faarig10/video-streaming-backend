55c23de17f07bae9bcd81d6b8c68f135087dac20
Yash Shukla <shuklayash10.97@gmail.com>
2025-01-16 13:43:30 +0530

    Ckoudinart utility function for cloudinary uploads

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+// *Cloudinary utilty function for uploading files in cloudinary
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+//* Configuring Cloudinary
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded on cloudinary. File src: " + response.url);
+    //* once the file is uploaded, we would delete it from our server as well
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
